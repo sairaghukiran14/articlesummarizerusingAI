f31d7094bac1b17db19dd977c4cb34d035c3e6c2
Avula Sai Raghu Kiran <avulasairaghukiran1998@gmail.com>
2023-08-01 12:05:08 +0530

    first commit

---
+++ SumzAI/src/services/article.js
@@ -0,0 +1,28 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+// const apikey = import.meta.env.VITE_RAPID_SUMZ_KEY;
+const apikey = "f071b05532mshf470bb174488acfp1523d9jsn03a2f20c4c0a";
+
+export const articleApi = createApi({
+  reducerPath: "articleApi",
+  baseQuery: fetchBaseQuery({
+    baseUrl: "https://article-extractor-and-summarizer.p.rapidapi.com/",
+    prepareHeaders: (headers) => {
+      headers.set("X-RapidAPI-Key", apikey);
+      headers.set(
+        "X-RapidAPI-Host",
+        "article-extractor-and-summarizer.p.rapidapi.com"
+      );
+      return headers;
+    },
+  }),
+
+  endpoints: (builder) => ({
+    getSummary: builder.query({
+      query: (params) =>
+        `/summarize?url=${encodeURIComponent(params.articleUrl)}&length=3`,
+    }),
+  }),
+});
+
+export const { useLazyGetSummaryQuery } = articleApi;
